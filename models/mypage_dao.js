047bfc550d680f417eecddaf5cc7a1c52e43b1f8
xixgyxx1 <haeyeon1796@gmail.com>
2024-10-22 14:35:13 +0900

    MOD prefer

--- models/mypage_dao.js
+++ models/mypage_dao.js
@@ -10,8 +10,8 @@ const getMyPageDetails = async (pk) => {
    'cafe_address', cafes.location_address
   )) AS likeList
    FROM users
-   LEFT JOIN cafe_likes ON users.id = cafe_likes.user_id
-   LEFT JOIN cafes ON cafe_likes.cafe_id = cafes.id
+   LEFT JOIN user_likes ON users.id = user_likes.user_id
+   LEFT JOIN cafes ON user_likes.cafe_id = cafes.id
    WHERE users.id = ?
    GROUP BY users.id
     `, [pk])
